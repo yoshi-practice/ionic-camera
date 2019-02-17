50431ecca512137d8d65795175f6ededbd1628dc
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-02-17 15:46:29 +0900

    [Update] First commit.

---
+++ src/app/camera-page/camera-page.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { CameraPagePage } from './camera-page.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CameraPagePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [CameraPagePage]
+})
+export class CameraPagePageModule {}
---
+++ src/app/camera-page/camera-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-camera-page',
+  templateUrl: './camera-page.page.html',
+  styleUrls: ['./camera-page.page.scss'],
+})
+export class CameraPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
